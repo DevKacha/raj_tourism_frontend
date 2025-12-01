dbe0b857668b5866f9c207c33be17343b445bb3c
dev Kacha <kachadev60@gmail.com>
2025-12-01 21:21:04 +0530

    Final updates before Render deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -101,15 +101,7 @@ export default function Footer() {
                   <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-600 group-hover:w-full transition-all duration-300"></span>
                 </span>
               </a>
-              <a
-                href="/services"
-                className="block text-sm text-gray-600 hover:text-blue-600 transition-colors relative group"
-              >
-                <span className="relative">
-                  Our Services
-                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-600 group-hover:w-full transition-all duration-300"></span>
-                </span>
-              </a>
+
               <a
                 href="/contact"
                 className="block text-sm text-gray-600 hover:text-blue-600 transition-colors relative group"
